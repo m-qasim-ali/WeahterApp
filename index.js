b5d084986ce7718875e1336e74d6f1adb1a02d22
m-qasim-ali <muhammadqasimali96@gmail.com>
2023-05-17 01:00:40 +0500

    updated light theme again

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // lights colors
 const lightTheme = {
   primary: '#0f172a',
-  secondary: '#ffffff',
-  tertiary: '#f8f8f8',
+  secondary: '#fafafa',
+  tertiary: '#f4f4f4',
   accent: '#64748b',
 };
 
@@ -13,12 +13,31 @@ const darkTheme = {
   accent: '#94a3b8',
 };
 
-const lightmodeicon = `<svg width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" fill="none" xmlns="http://www.w3.org/2000/svg">
+const lightmodeicon = `<svg width="24" height="24" viewBox="0 0 24 24" class="theme-icon" stroke="currentColor" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M5 11.4597C5 15.6241 8.4742 19 12.7598 19C16.0591 19 18.8774 16.9993 20 14.1783C19.1109 14.5841 18.1181 14.8109 17.0709 14.8109C13.2614 14.8109 10.1732 11.8101 10.1732 8.1084C10.1732 6.56025 10.7134 5.13471 11.6205 4C7.87502 4.5355 5 7.67132 5 11.4597Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
 </svg>`;
 
-const darkmodeicon = `<svg width="24" height="24" viewBox="0 0 24 24" class="theme-icon" stroke="currentColor" fill="none" xmlns="http://www.w3.org/2000/svg"><pathd="M17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12C7 9.23858 9.23858 7 12 7C14.7614 7 17 9.23858 17 12Z"stroke-width="1.5"></pathd=>
-<path d="M18.3117 5.68834L18.4286 5.57143M5.57144 18.4286L5.68832 18.3117M12 3.07394V3M12 21V20.9261M3.07394 12H3M21 12H20.9261M5.68831 5.68834L5.5714 5.57143M18.4286 18.4286L18.3117 18.3117" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>`;
+const darkmodeicon = `<svg
+width="24"
+height="24"
+viewBox="0 0 24 24"
+class="theme-icon"
+stroke="currentColor"
+fill="none"
+xmlns="http://www.w3.org/2000/svg"
+>
+<path
+  d="M17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12C7 9.23858 9.23858 7 12 7C14.7614 7 17 9.23858 17 12Z"
+  stroke-width="1.5"
+></path>
+
+<path
+  d="M18.3117 5.68834L18.4286 5.57143M5.57144 18.4286L5.68832 18.3117M12 3.07394V3M12 21V20.9261M3.07394 12H3M21 12H20.9261M5.68831 5.68834L5.5714 5.57143M18.4286 18.4286L18.3117 18.3117"
+  stroke-width="1.5"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+></path>
+</svg>`;
 
 // DOM elements
 const themeBox = document.querySelector('#theme-box');
